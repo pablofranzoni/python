87d9a5812af10ae1e682ddcb6a67eec6406dfdcd
pablofranzoni <pablo.franzoni@gmail.com>
2025-04-09 00:00:10 -0300

    Se reemplaza por funcion de itertools.groupby

--- prueba.py
+++ prueba.py
@@ -1,19 +1,6 @@
-ant=''
-count = 0
-str = "aaaabbbcca"
-list_elem = list()
-
-ant = str[1:2]
-for s in str:
-    if ant != s:
-        t = (ant, count)
-        list_elem.append(t)
-        count = 0
-        ant = s
-    count += 1
-
-t = (ant, count)
-list_elem.append(t)
-print(tuple(list_elem))    
+from itertools import *
+h = [(k, len(list(g))) for k, g in groupby('aaaabbbcca')]
+ 
+print(tuple(h))
     
       
